cbb60e64eb56ef99dec9590099520bf81f38a51b
Tin Myo Win <121311245+tmyowin@users.noreply.github.com>
2023-01-06 10:48:34 +0630

    5_doWhile_Loop

---
+++ 5_doWhile_Loop.cpp
@@ -0,0 +1,13 @@
+//
+// Created by MIS on 15-Nov-22.
+//
+#include "stdio.h"
+int main(){
+    int i=100;
+
+    do{
+        printf("This is diploma in computing level_1:");
+    } while (i>200);
+
+    return 0;
+}
