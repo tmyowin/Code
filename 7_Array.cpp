2e5cbcf958cdfe14ad5b9d880d9780065b845b51
Tin Myo Win <121311245+tmyowin@users.noreply.github.com>
2023-01-06 10:50:41 +0630

    7_Array

---
+++ 7_Array.cpp
@@ -0,0 +1,16 @@
+//
+// Created by MIS on 21-Nov-22.
+//
+#include "stdio.h"
+
+int main(){
+
+   int myArray[10];
+
+
+   for(int i=0;i<10;i++){
+       printf("%d\n",myArray[i]);
+   }
+
+    return 0;
+}
